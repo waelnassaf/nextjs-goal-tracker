5996e81ab579786372a45624dd0ea6f193125ebc
Wael <waelassaf121@gmail.com>
2024-06-11 02:24:05 +0300

    Testing Push

---
+++ lib/data.ts
@@ -0,0 +1,102 @@
+export const goals = [
+  {
+    title: "Career",
+    goals: [
+      "Get a promotion",
+      "Switch to a new industry",
+      "Start a business",
+      "Develop a new skill",
+      "Network with industry leaders",
+    ],
+  },
+  {
+    title: "Health",
+    goals: [
+      "Lose 10 pounds",
+      "Run a marathon",
+      "Eat more vegetables",
+      "Exercise three times a week",
+      "Quit smoking",
+    ],
+  },
+  {
+    title: "Education",
+    goals: [
+      "Earn a degree",
+      "Complete an online course",
+      "Read 20 books in a year",
+      "Learn a new language",
+      "Attend a workshop or seminar",
+    ],
+  },
+  {
+    title: "Finance",
+    goals: [
+      "Save $10,000",
+      "Invest in stocks",
+      "Create a budget",
+      "Pay off debt",
+      "Build an emergency fund",
+    ],
+  },
+  {
+    title: "Relationship",
+    goals: [
+      "Get engaged",
+      "Plan a vacation together",
+      "Buy a house together",
+      "Improve communication skills",
+      "Spend more quality time together",
+    ],
+  },
+  {
+    title: "Personal Development",
+    goals: [
+      "Practice mindfulness daily",
+      "Start a new hobby",
+      "Volunteer regularly",
+      "Write a journal",
+      "Set and achieve a major personal milestone",
+    ],
+  },
+  {
+    title: "Travel",
+    goals: [
+      "Visit a new country",
+      "Plan a road trip",
+      "Go on a solo trip",
+      "Explore a local destination",
+      "Experience a different culture",
+    ],
+  },
+  {
+    title: "Home Improvement",
+    goals: [
+      "Renovate the kitchen",
+      "Create a garden",
+      "Organize the garage",
+      "Upgrade home security",
+      "Decorate the living room",
+    ],
+  },
+  {
+    title: "Creativity",
+    goals: [
+      "Write a book",
+      "Learn to play an instrument",
+      "Take a photography course",
+      "Start a blog",
+      "Create a piece of art",
+    ],
+  },
+  {
+    title: "Spiritual",
+    goals: [
+      "Meditate daily",
+      "Attend a spiritual retreat",
+      "Read spiritual texts",
+      "Practice gratitude",
+      "Join a spiritual community",
+    ],
+  },
+];
---
+++ lib/fonts.ts
@@ -0,0 +1,3 @@
+import { Happy_Monkey } from "next/font/google";
+
+export const HM = Happy_Monkey({ weight: "400", subsets: ["latin"] });
