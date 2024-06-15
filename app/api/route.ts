1cd41c7e6bff561359429864ee76f2b576a7fa68
Wael <waelassaf121@gmail.com>
2024-06-15 07:31:47 +0300

    Fix build 3

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse } from "next/server";
-import { getFullUserData } from "@/server/actions";
-
-// This is an api Route to view a sample of the db return
-export async function GET() {
-  const res = await getFullUserData("666980120b5c919fd83b6b1d");
-  return NextResponse.json(res);
-}
+// import { NextResponse } from "next/server";
+// import { getFullUserData } from "@/server/actions";
+//
+// // This is an api Route to view a sample of the db return
+// export async function GET() {
+//   const res = await getFullUserData("666980120b5c919fd83b6b1d");
+//   return NextResponse.json(res);
+// }
