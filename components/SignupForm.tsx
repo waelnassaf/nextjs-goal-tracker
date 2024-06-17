016f435494e318adade06fbcbbdfb3ee95a64f5e
Wael <waelassaf121@gmail.com>
2024-06-18 02:22:37 +0300

    Change captcha spacing

--- components/SignupForm.tsx
+++ components/SignupForm.tsx
@@ -137,7 +137,7 @@ const SignupForm: NextPage = () => {
               <BsKey className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
-          <div className="mt-4">
+          <div className="my-4">
             <label
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
               htmlFor="goalsEndDate"
